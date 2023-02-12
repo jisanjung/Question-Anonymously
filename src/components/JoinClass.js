c211b9fcfcd4d40dff35a691447cd5bf27df4019
artless <jayanlas00@gmail.com>
2023-02-11 19:08:15 -0500

    all pages made

---
+++ src/components/JoinClass.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const JoinClass = (props) => {
+
+    if(props.name === "Teacher"){
+        return (
+            <div className="card text-center bg-light">
+                <h3>Hello {props.name} </h3>
+                <br/>
+                Input your name<br/>
+                <input type="text"></input>
+                <br/>
+                Input your password<br/>
+                <input type="text"></input>
+                <br/>
+                <Link  to="/classTeacher" className='btn btn-primary'>Generate Class Code</Link>
+            </div>
+            
+          )
+    }
+
+  return (
+    <div className="card text-center bg-light">
+        <h3>Hello {props.name} </h3>
+        <br/>
+        Input your name<br/>
+        <input type="text"></input>
+        <br/>
+        Input your class code<br/>
+        <input type="text"></input>
+        <br/>
+        <Link to="/classStudent" type="button" className="btn btn-primary">Join Classroom</Link>
+    </div>
+  )
+}
+
+export default JoinClass
