fbeb303caa78b72a8e86ccfdb2143a08d96395ac
artless <jayanlas00@gmail.com>
2023-02-12 08:38:53 -0500

    app file doesnt do firebase

--- src/App.js
+++ src/App.js
@@ -5,35 +5,19 @@ import StudentViewPage from './pages/StudentViewPage';
 import TeacherViewPage from './pages/TeacherViewPage';
 import ClassroomStudentView from './pages/ClassroomStudentView';
 import ClassroomTeacherView from './pages/ClassroomTeacherView';
-import {useState, useEffect} from 'react';
-import {db} from './firebase_setup/firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import ViewData from './components/ViewData';
 
 function App() {
-  const [classrooms, setClassroom] = useState([]);
-  const teachersCollectionRef = collection(db, "Classrooms")
-  useEffect(() => {
-    const getTeachers = async () => {
-      const data = await getDocs(teachersCollectionRef);
-      console.log(
-        setClassroom(data.docs.map((doc) => ({...doc.data()})))
-        );
-      console.log(data);
-    }
-
-    getTeachers()
-  }, []
-  )
 
   return (
     <div>
+      <ViewData/>
       <Routes>
         <Route path='/' element={<RoleSelectPage/>}/>
         <Route path='/student' element={<StudentViewPage/>}/>
         <Route path='/teacher' element={<TeacherViewPage/>}/>
         <Route path='/classTeacher' element={<ClassroomTeacherView/>}/>
         <Route path='/classStudent' element={<ClassroomStudentView/>}/>
-        
       </Routes>
     </div>
   );
