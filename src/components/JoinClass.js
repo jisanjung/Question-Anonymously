9eee2ed5a584a9976be7538491554bcca6c1b461
artless <jayanlas00@gmail.com>
2024-08-16 08:54:11 -0400

    added a bit of styling and fixed up dependencies

--- src/components/JoinClass.js
+++ src/components/JoinClass.js
@@ -3,22 +3,21 @@ import {Link} from 'react-router-dom'
 
 const JoinClass = (props) => {
 
-    if(props.name === "Teacher"){
-        return (
-            <div className="card text-center bg-light">
-                <h3>Hello {props.name} </h3>
-                <br/>
-                Input your name<br/>
-                <input type="text"></input>
-                <br/>
-                Input your password<br/>
-                <input type="text"></input>
-                <br/>
-                <Link  to="/classTeacher" className='btn btn-primary'>Generate Class Code</Link>
-            </div>
-            
-          )
-    }
+  if(props.name === "Teacher"){
+    return (
+      <div className="card text-center bg-light">
+        <h3>Hello {props.name} </h3>
+        <br/>
+        Input your name<br/>
+        <input type="text"></input>
+        <br/>
+        Input your password<br/>
+        <input type="text"></input>
+        <br/>
+        <Link  to="/classTeacher" className='btn btn-primary'>Generate Class Code</Link>
+      </div>   
+    )
+  }
 
   return (
     <div className="card text-center bg-light">
--- src/pages/TeacherViewPage.js
+++ src/pages/TeacherViewPage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import JoinClass  from '../components/JoinClass'
 
 const TeacherViewPage = () => {
 
