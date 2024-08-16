afb68807d9211dc07971ebf108b9018e24f7f5c8
artless <jayanlas00@gmail.com>
2024-08-16 09:32:25 -0400

    student form resets after submitting question

--- src/components/JoinClass.js
+++ src/components/JoinClass.js
@@ -23,25 +23,31 @@ const JoinClass = (props) => {
     }
 
   return (
+    //FOR STUDENTS
     <div className="card text-center bg-light">
         <h3>Hello {props.name} </h3>
         <br/>
         Class code<br/>
-        <input type="text" onChange={e => setCode(e.target.value)}></input>
+        <input type="text" id="studentCode" placeholder="ex: 8675309" onChange={e => setCode(e.target.value)}></input>
         <br/>
         <br/>
         What's your name<br/>
-        <input type="text" onChange={e => setName(e.target.value)}></input>
+        <input type="text" id="studentName" placeholder="ex: John Doe" onChange={e => setName(e.target.value)}></input>
         <br/>
         Ask a question<br/>
-        <input type="text" onChange={e => setQuestion(e.target.value)}></input>
+        <input type="text" id="studentQuestion" placeholder="ex: what did you say?" onChange={e => setQuestion(e.target.value)}></input>
         <br/>
-        <button type='button' onClick={async () => {
+        <button type='button' className="btn btn-primary" onClick={async () => {
           // Add a new document with a generated id.
           const docRef = await addDoc(collection(db, code), {
             name: name,
             question: question
           });
+
+          document.getElementById("studentCode").value = null;
+          document.getElementById("studentName").value = null;
+          document.getElementById("studentQuestion").value = null;
+
           console.log("Document written with ID: ", docRef.id);
         }}>Ask in class</button>
     </div>
