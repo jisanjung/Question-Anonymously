2ae4621459f6a23b09ba6300fe3d57b41d2cda05
Jisan Jung <jjjung13@gmail.com>
2024-08-17 22:59:55 -0500

    fixed width for question form component

--- src/components/QuestionForm.jsx
+++ src/components/QuestionForm.jsx
@@ -12,22 +12,24 @@ const QuestionForm = () => {
     
   return (
     <>
-    {questionSent && 
-    <Alert type='success' setVisible={setQuestionSent}>Question sent!</Alert>}
-    <form onSubmit={(e) => onQuestionSubmit(e)}>
-        <div className="form-floating mb-3 mt-3">
-            <textarea 
-                className="form-control" 
-                placeholder="Your question..." 
-                id="floatingTextarea2" 
-                style={{ height: '150px' }}
-            ></textarea>
-            <label htmlFor="floatingTextarea2">Your question...</label>
-        </div>
-        <div className='d-grid gap-2'>
-            <button className='btn btn-info text-white' type='submit'>Ask</button>
-        </div>
-    </form>
+    <div id='questionForm'>
+        {questionSent && 
+        <Alert type='success' setVisible={setQuestionSent}>Question sent!</Alert>}
+        <form onSubmit={(e) => onQuestionSubmit(e)}>
+            <div className="form-floating mb-3 mt-3">
+                <textarea 
+                    className="form-control" 
+                    placeholder="Your question..." 
+                    id="floatingTextarea2" 
+                    style={{ height: '150px' }}
+                ></textarea>
+                <label htmlFor="floatingTextarea2">Your question...</label>
+            </div>
+            <div className='d-grid gap-2'>
+                <button className='btn btn-info text-white' type='submit'>Ask</button>
+            </div>
+        </form>
+    </div>
     </>
   )
 }
