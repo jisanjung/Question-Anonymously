5f7a7031ad3b1740992ae0db5123f92b51d46b64
Jisan Jung <jjjung13@gmail.com>
2024-08-24 19:39:16 -0500

    sort questions before setting ui

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { timeFromNow } from '../util/helper';
+import { generateNumberFromId } from '../util/helper';
 
 const QuestionCard = ({ payload, index }) => {
   return (
     <>
         <div className="card mb-3 cursor-pointer question-card">
             <div className="card-body">
-                <h5 className="card-title">Question {index + 1}</h5>
-                <p className="card-text">{payload?.question}</p>
+                <p className="card-title">Question #{generateNumberFromId(payload?.id)}</p>
+                <h5 className="card-text fw-bold">{payload?.question}</h5>
                 <p className="card-text">
                   <small className="text-body-secondary">
                     {payload?.timestamp ? timeFromNow(payload?.timestamp) : 'N/A'}
